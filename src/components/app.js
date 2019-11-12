ae7793ee98bd691a0f1e57b2e0fb0c01c0adbf6f
dstephenson16 <che.dami@gmail.com>
2019-11-12 11:32:18 -0700

    Added the day block component

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Weekday from "./weekday";
+import DayBlock from "./dayBlock";
 
 export default class App extends Component {
   renderDays = () => {
@@ -23,7 +24,7 @@ export default class App extends Component {
           </div>
 
           <div className="blocks-wrapper">
-
+            <DayBlock date="27" />
           </div>
 
         </div>
---
+++ src/components/dayBlock.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+export default class DayBlock extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="day-block">
+                <div className="date">{this.props.date}</div>
+                <textarea className="calendar-input" />
+            </div>
+        )
+    }
+}
